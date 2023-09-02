0d5f2ae4f7358ffcd97a2bee265a55eaa672b28b
raghujhts13 <raghuunaathan7@gmail.com>
2023-09-02 17:37:34 +0530

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify, session
+from flask import Flask, request, render_template, redirect, url_for, jsonify, session, Response
 import os
+import json
+import time
 from multiprocessing import Pool
 from tqdm import tqdm
 import glob
@@ -7,7 +9,7 @@ from typing import List
 from dotenv import load_dotenv
 import pandas as pd
 # model imports
-from langchain.llms import GPT4All
+from langchain.llms import GPT4All, LlamaCpp
 from langchain.chains import RetrievalQA
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 # model imports
@@ -26,9 +28,13 @@ import chromadb
 nv_path = os.path.join(os.path.dirname(__file__), '.', '.env')
 load_dotenv(dotenv_path=nv_path)
 
+session_id = 0
 llm = None
 filepath = os.environ['FILE_PATH']
 file_list = os.listdir(filepath)
+file_paths = [f'static/files/{i}' for i in file_list]
+questions=[]
+answers=[]
 # Load environment variables
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 source_directory = os.environ.get('SOURCE_DIRECTORY', '../application/static/files')
@@ -138,6 +144,7 @@ def ingester():
 # ===================================================================
 def main(query):
     # Parse the command line arguments
+    print('entered gpt4j function')
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS , path=persist_directory)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
@@ -145,13 +152,15 @@ def main(query):
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
+    global llm
     match model_type:
+        case "LlamaCpp":
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case "GPT4All":
-            global llm
             if llm:
                 pass
             else:
-                llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+                llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj',n_threads=4, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
             # raise exception if model_type is not supported
             raise Exception(f"Model type {model_type} is not supported. Please choose GPT4All")
@@ -164,6 +173,7 @@ def main(query):
 # ===================================================================
 
 def delete_document_with_embeddings(document):
+    print('delete documents entered')
     collection = chroma_client.get_collection(name="langchain")
     metadata_list = collection.get()['metadatas']
     for metadata in metadata_list:
@@ -181,10 +191,19 @@ def delete_document_with_embeddings(document):
         print(f"{file_to_delete} does not exist.")
     return 'file has been deleted'
 
-
+data=[]
+if os.path.exists(os.environ['json_file_path']):
+    # If the file exists, open it for appending data
+    with open(os.environ['json_file_path'], 'r') as file:
+        data1 = json.load(file)
+        for key in data1.keys():
+            if isinstance(data1[key], dict):
+                if data1[key]:
+                    data.append(data1[key])
+data = len(data)
 @app.get('/')
 def index():
-    return render_template('index.html',files=file_list)
+    return render_template('index.html',files=file_list, filepaths=file_paths,chats=data)
 @app.route('/upload',methods=['POST'])
 async def upload():
     uploaded_file=request.files['file']
@@ -208,12 +227,117 @@ def delete_document():
 
 @app.route('/qanda',methods=['POST'])
 def qanda():
+    global questions
+    global answers
     query = request.get_json()
     try:
-        return main(query)
+        query = f'return the output as a html. Question:{query["query"]}'
+        print('entered qanda')
+        result = main(query)
+        questions.append(query)
+        answers.append(result)
+        return result
     except Exception as e:
         print(e)
         return 'sorry, I am not able to answer your query'
+@app.route('/session_check',methods=['POST'])
+def session_check():
+    global questions
+    global answers
+    curr_session = request.get_json()
+    curr_session = curr_session['session']+1
+    print(curr_session)
+    if curr_session<=5:
+        # Check if the JSON file exists
+        if os.path.exists(os.environ['json_file_path']):
+            # If the file exists, open it for appending data
+            with open(os.environ['json_file_path'], 'r') as file:
+                data = json.load(file)
+            
+            if str(curr_session) in data:
+                data[str(curr_session)].update(dict(zip(questions,answers)))
+            else:
+                # If the file doesn't exist, create an empty data dictionary
+                data[str(curr_session)] = dict(zip(questions,answers))
+        else:
+            data = {}
+            # If the file doesn't exist, create an empty data dictionary
+            data[str(curr_session)] = dict(zip(questions,answers))
+        # After modifying the data, save it back to the JSON file
+        with open(os.environ['json_file_path'], 'w') as file:
+            json.dump(data, file, indent=4)
+        questions,answers = [],[]
+        return 'successfully saved the chat history'
+    else:
+        return 'limit exceeded'
+
+@app.route('/delete_session',methods=['POST'])
+def delete_session():
+    content = request.get_json()
+    key = content['session_id']
+    key = key.strip()
+    data = []
+    with open(os.environ['json_file_path'], 'r') as file:
+        data = json.load(file)
+    if key in data.keys():
+        del data[key]
+    with open(os.environ['json_file_path'], 'w') as file:
+        json.dump(data, file, indent=4)
+    return 'deleted chat'
+
+@app.route('/save_session',methods=['POST'])
+def save_session():
+    global questions
+    global answers
+    content = request.get_json()
+    key = content['session_id']
+    key = key.strip()
+    data = []
+    with open(os.environ['json_file_path'], 'r') as file:
+        data = json.load(file)
+    if key in data.keys():
+        data[key].update(dict(zip(questions,answers)))
+        questions,answers = [],[]
+    else:
+        data[key] = dict(zip(questions,answers))
+        questions,answers = [],[]
+    with open(os.environ['json_file_path'], 'w') as file:
+        json.dump(data, file, indent=4)
+    print('worked till here')
+    return 'deleted chat'
+
+@app.route('/export_session',methods=['POST'])
+def export_session():
+    global questions
+    global answers
+    content = request.get_json()
+    key = content['session_id']
+    key = key.strip()
+    with open(os.environ['json_file_path'], 'r') as file:
+        data = json.load(file)
+    if key in data.keys():
+        data[key].update(dict(zip(questions,answers)))
+    else:
+        data[key] = dict(zip(questions,answers))
+    with open(os.environ['json_file_path'], 'w') as file:
+        json.dump(data, file, indent=4)
+    with open(f'{os.environ["OUTPUT_PATH"]}/chatbkp_{str(time.time())}.json','w') as file:
+        json.dump(data,file,indent=4)
+    return 'session saved'
+
+@app.route('/load_session',methods=['POST'])
+def load_session():
+    content = request.get_json()
+    key = content['session_id'].replace('session','')
+    key = key.strip()
+    print(key)
+    data = []
+    if os.path.exists(os.environ['json_file_path']):
+        with open(os.environ['json_file_path'], 'r') as file:
+            data = json.load(file)
+    if key in data.keys():
+        print(data[key])
+        return data[key]
 
 if __name__ == '__main__':
     app.run(debug=True)
